e19b1089d2dd713c177968285bd589cb7bbb1174
litian <zlt@umich.edu>
2019-11-24 11:53:07 -0500

    modified test case

--- tests/testthat/test-summary_linearReg.R
+++ tests/testthat/test-summary_linearReg.R
@@ -8,3 +8,11 @@ test_that("F_statistic equal", {
   model_me = linearReg(Y,X)
   expect_equal(unname(summary(model_lm)$fstatistic[1]), summary_linearReg(model_me))
 })
+
+test_that("when sample size smaller or equal to 7", {
+  Y = rnorm(7)
+  X = Y+rnorm(7,sd=3)
+  model_lm = lm(Y~X)
+  model_me = linearReg(Y,X)
+  expect_equal(unname(model_lm$coefficients), unname(model_me$coefficients))
+})
